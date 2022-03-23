6ff3ebc587015a9aed24abfa6ddc6e9ded5802c3
Olgamtz05 <100552290+Olgamtz05@users.noreply.github.com>
2022-03-23 14:26:53 -0600

    Probabilidad
    Ejercicio 1 sobre probabilidad

---
+++ Prob1.R
@@ -0,0 +1,13 @@
+#Probabilidad 
+#Pnorm = número 
+# Mean = a comparar probabilidad
+# sqrt= desviación estandar
+pnorm(48, mean= 50, sd= sqrt(25))  #Ejercicio propuesto por programador
+pnorm(63, mean=66, sd=sqrt(25)) #Ejercicio prueba 
+pnorm(100, mean=70, sd=sqrt(25)) #Ejercicio prueba
+pnorm(200, mean=100, sd=sqrt(40)) #Ejercicio prueba
+
+
+#Bibliografía:
+# Lopez, J. (2020). RPubs Distribuciones de probabilidad. 
+# Recuperado de https://rpubs.com/CarlosLopez123/631357
