792568f08eb6417b9afde2a4cdc7a511d3a7dbf4
Olgamtz05 <100552290+Olgamtz05@users.noreply.github.com>
2022-03-23 14:24:30 -0600

    Distribuciones
    Ejercicio 1 de distribuciones: tiempo de llegada a una escuela primaria.

---
+++ Distribuciones.R
@@ -0,0 +1,16 @@
+#Probabilidad de que 35 alumnos lleguen en 15 minutos 
+#dpois(x, lamda)
+#x= alumnos que esperemos que lleguen (35)
+#lambda= minutos (15)
+dpois(35, 15)
+
+#Probabilidad de que 35 alumnos lleguen en 5 minutos
+dpois(35, 5)
+
+#Probabilidad de que 34 alumnos lleguen en 5 minutos
+dpois(34, 5)
+
+#Conclusión
+#Mientras más tiempo se de, hay menor probabilidad de que 
+#existan aglomeraciones. Se debe abrir el portón con tiempo
+#para evitar aglomeraciones.
